b1c953ef6658adb170069aefc6feaa4840b1a003
asiek171 <danko.joanna@gmail.com>
2024-12-28 15:44:40 +0100

    update .gitignore

---
+++ Application/Services/Strategy/Factory/IntegrationStrategyFactory.cs
@@ -0,0 +1,29 @@
+﻿using Application.Enums;
+using Application.Services.Strategy.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Services.Strategy.Factory
+{
+    public class IntegrationStrategyFactory
+    {
+        private readonly IServiceProvider _serviceProvider;
+        public IntegrationStrategyFactory(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public IIntegrationStrategy GetStrategy(SystemType systemType)
+        {
+            var client = _serviceProvider.GetRequiredService<SpoonacularStrategy>();
+
+            return systemType switch
+            {
+                SystemType.Spoonacular => _serviceProvider.GetRequiredService<SpoonacularStrategy>(),
+                SystemType.Nutritionix => _serviceProvider.GetRequiredService<NutritionixStrategy>(),
+                SystemType.Edamam => _serviceProvider.GetRequiredService<EdamamStrategy>(),
+                SystemType.TheMealDb => _serviceProvider.GetRequiredService<TheMealDbStrategy>(),
+                _ => throw new ArgumentException("Not implemented tpe of integration")
+            };
+        }
+    }
+}
