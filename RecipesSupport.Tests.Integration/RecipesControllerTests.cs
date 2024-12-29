017877ae829940d4972b856fdef7e33b63f7363c
asiek171 <danko.joanna@gmail.com>
2024-12-29 22:01:53 +0100

    run integration test

--- RecipesSupport.Tests.Integration/RecipesControllerTests.cs
+++ RecipesSupport.Tests.Integration/RecipesControllerTests.cs
@@ -1,15 +1,14 @@
 using RecipesSupport.Tests.Integration.Base;
-using Xunit;
 
 namespace RecipesSupport.Tests.Integration.Controllers
 {
     public class RecipesControllerTests : IntegrationTestBase
     {
-        [Fact]
+        [Test]
         public async Task GetItems_ReturnsOkResponse()
         {
             // Arrange
-            var requestUri = "/Recipes/searchByIngredients?ingredients=butter";
+            var requestUri = "Recipes/searchByIngredients?ingredients=butter";
 
             // Act
             var response = await TestClient.GetAsync(requestUri);
